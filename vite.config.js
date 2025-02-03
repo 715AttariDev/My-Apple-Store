4d1b6ffa1a1315525bc44232a763ff29c7eaa0e4
715AttariDev <attariabdulrahim3@gmail.com>
2025-02-03 19:09:06 +0500

    add code

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/My-Apple-Store/',
   server: {
     host: '0.0.0.0', // Allow connections from any device on the network
     port: 5173, // Default Vite port, you can change it if needed
